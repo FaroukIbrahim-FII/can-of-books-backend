a9c973fb0ea2d3a183c5daec3bd3256d654abf3c
faroukibrahim-fii <farouk94i@yahoo.com>
2021-09-15 17:03:40 +0300

    solving lab14

--- server.js
+++ server.js
@@ -1,3 +1,179 @@
+// 'use strict';
+
+// const express = require('express');
+// const cors = require('cors');
+// require('dotenv').config();
+
+// const server = express();
+// server.use(cors());
+
+// const PORT = process.env.PORT;
+// server.use(express.json());
+
+// //MongoDB
+// const mongoose = require('mongoose');
+
+// let bookModel;
+// main().catch(err => console.log(err));
+
+// async function main() {
+//   await mongoose.connect('mongodb://localhost:27017/301-books');
+// //   await mongoose.connect(process.env.MONGO_URL);
+
+//   const bookSchema = new mongoose.Schema({
+//     bookTitle: String,
+//     bookDescription: String,
+//     bookStatus: String,
+//     bookEmail: String
+
+//   });
+
+//   bookModel = mongoose.model('book', bookSchema);
+
+// //   seedData();
+// }
+
+// //seeding a data function 
+
+// async function seedData()
+// {
+//     const HarryPotter1 = new bookModel({
+//                 bookTitle: `Harry Potter and the Philosopher's Stone`,
+//                 bookDescription: `Harry Potter and the Philosopher's Stone is a fantasy novel written by British author J. K. Rowling.`,
+//                 bookStatus: 'available',
+//                 ownerEmail: 'farouk9435@gmail.com'
+//             });
+
+//             const HarryPotter2 = new bookModel({
+//                 bookTitle: `Harry Potter and the Chamber of Secrets`,
+//                 bookDescription: `Harry Potter and the Chamber of Secrets is a fantasy novel written by British author J. K. Rowling and the second novel in the Harry Potter series.`,
+//                 bookStatus: 'available',
+//                 ownerEmail: 'farouk9435@gmail.com'
+//             });
+
+//             const HarryPotter3 = new bookModel({
+//                 bookTitle: `Harry Potter and the Prisoner of Azkaban`,
+//                 bookDescription: `Harry Potter and the Prisoner of Azkaban is a fantasy novel written by British author J. K. Rowling and is the third in the Harry Potter series. The book follows Harry Potter, a young wizard, in his third year at Hogwarts School of Witchcraft and Wizardry.`,
+//                 bookStatus: 'available',
+//                 ownerEmail: 'farouk9435@gmail.com'
+//             });
+
+
+//             await HarryPotter1.save();
+//             await HarryPotter2.save();
+//             await HarryPotter3.save();
+// }
+
+
+// //Routes
+// server.get('/', homeHandler);
+// server.get('/book',getBooksHandler);
+// server.post('/books',addBook);
+// server.delete('/books/:id',deleteBook);
+// server.put('/books/:id',updatebook)
+
+// //Functions Handlers
+// function homeHandler(req,res){
+
+//     res.send('Home Page');
+// }
+
+// function getBooksHandler(req,res){
+//     //send fav cats list (email)
+//     const email = req.query.email;
+//     bookModel.find({ownerEmail:email},(err,result)=>{
+//         if(err)
+//         {
+//             console.log(err);
+//         }
+//         else
+//         {
+//             res.send(result);
+//         }
+//     })
+// }
+
+
+
+
+// async function addBook(req,res){
+//     console.log(req.body);
+//     const bookTitle = req.body.bookTitle;
+//     const bookDescription = req.body.bookDescription;
+//     const bookStatus = req.body.bookStatus;
+//     const bookEmail = req.body.ownerEmail;
+//     // const {bookTitle, bookDescription, bookStatus,bookEmail} = req.body;
+//     await bookModel.create({ 
+//         bookTitle: bookTitle,
+//         bookDescription: bookDescription,
+//         bookStatus: bookStatus,
+//         bookEmail :bookEmail
+//     });
+
+//     // await KittenModel.create({catName,catBreed,ownerEmail});
+
+//     bookModel.find({ownerEmail:bookEmail},(err,result)=>{
+//         if(err)
+//         {
+//             console.log(err);
+//         }
+//         else
+//         {
+//             res.send(result);
+//         }
+//     })
+
+// }
+
+
+// function deleteBook(req,res){
+//     const bookId = req.params.id;
+//     const email = req.query.email;
+//     bookModel.deleteOne({_id:bookId},(err,result)=>{
+
+//         bookModel.find({ownerEmail:email},(err,result)=>{
+//             if(err)
+//             {
+//                 console.log(err);
+//             }
+//             else
+//             {
+//                 res.send(result);
+//             }
+//         })
+
+//     })
+
+
+// }
+
+// function updatebook(req,res) {
+//     const id = req.params.id;
+//     const {bookTitle,bookDescription,bookStatus,bookEmail} = req.body;
+
+//     KittenModel.findByIdAndUpdate(id,{bookTitle,bookDescription,bookStatus},(err,result)=>{
+//         KittenModel.find({ownerEmail:bookEmail},(err,result)=>{
+//             if(err)
+//             {
+//                 console.log(err);
+//             }
+//             else
+//             {
+//                 res.send(result);
+//             }
+//         })
+//     })
+// }
+
+
+
+
+// server.listen(PORT, () => {
+//     console.log(`listening on ${PORT}`);
+// })
+
+
+
 'use strict';
 
 const express = require('express');
@@ -9,7 +185,6 @@ server.use(cors());
 
 const PORT = process.env.PORT;
 server.use(express.json());
-
 //MongoDB
 const mongoose = require('mongoose');
 
@@ -17,62 +192,64 @@ let bookModel;
 main().catch(err => console.log(err));
 
 async function main() {
-//   await mongoose.connect('mongodb://localhost:27017/301-books');
-  await mongoose.connect(process.env.MONGO_URL);
+    // await mongoose.connect('mongodb://localhost:27017/301-books');
+    await mongoose.connect(process.env.MONGO_URL);
 
-  const bookSchema = new mongoose.Schema({
-    bookTitle: String,
-    bookDescription: String,
-    bookStatus: String,
-    bookEmail: String
 
-  });
+    const bookSchema = new mongoose.Schema({
+        bookTitle: String,
+        bookDescription: String,
+        bookStatus: String,
+        bookEmail: String
 
-  bookModel = mongoose.model('book', bookSchema);
+    });
 
-//   seedData();
+    bookModel = mongoose.model('book', bookSchema);
+
+    //   seedData();
 }
 
-//seeding a data function 
 
-async function seedData()
-{
+//seeding a data function 
+async function seedData() {
     const HarryPotter1 = new bookModel({
-                bookTitle: `Harry Potter and the Philosopher's Stone`,
-                bookDescription: `Harry Potter and the Philosopher's Stone is a fantasy novel written by British author J. K. Rowling.`,
-                bookStatus: 'available',
-                ownerEmail: 'farouk9435@gmail.com'
-            });
-        
-            const HarryPotter2 = new bookModel({
-                bookTitle: `Harry Potter and the Chamber of Secrets`,
-                bookDescription: `Harry Potter and the Chamber of Secrets is a fantasy novel written by British author J. K. Rowling and the second novel in the Harry Potter series.`,
-                bookStatus: 'available',
-                ownerEmail: 'farouk9435@gmail.com'
-            });
-        
-            const HarryPotter3 = new bookModel({
-                bookTitle: `Harry Potter and the Prisoner of Azkaban`,
-                bookDescription: `Harry Potter and the Prisoner of Azkaban is a fantasy novel written by British author J. K. Rowling and is the third in the Harry Potter series. The book follows Harry Potter, a young wizard, in his third year at Hogwarts School of Witchcraft and Wizardry.`,
-                bookStatus: 'available',
-                ownerEmail: 'farouk9435@gmail.com'
-            });
-        
-        
-            await HarryPotter1.save();
-            await HarryPotter2.save();
-            await HarryPotter3.save();
+
+        bookTitle: `Harry Potter and the Philosopher's Stone`,
+        bookDescription: `Harry Potter and the Philosopher's Stone is a fantasy novel written by British author J. K. Rowling.`,
+        bookStatus: 'Available',
+        ownerEmail: 'farouk9435@gmail.com'
+    });
+
+    const HarryPotter2 = new bookModel({
+        bookTitle: `Harry Potter and the Chamber of Secrets`,
+        bookDescription: `Harry Potter and the Chamber of Secrets is a fantasy novel written by British author J. K. Rowling and the second novel in the Harry Potter series.`,
+        bookStatus: 'Available',
+        ownerEmail: 'farouk9435@gmail.com'
+    });
+
+    const HarryPotter3 = new bookModel({
+        bookTitle: `Harry Potter and the Prisoner of Azkaban`,
+        bookDescription: `Harry Potter and the Prisoner of Azkaban is a fantasy novel written by British author J. K. Rowling and is the third in the Harry Potter series. The book follows Harry Potter, a young wizard, in his third year at Hogwarts School of Witchcraft and Wizardry.`,
+        bookStatus: 'Available',
+        ownerEmail: 'farouk9435@gmail.com'
+    });
+
+
+    await HarryPotter1.save();
+    await HarryPotter2.save();
+    await HarryPotter3.save();
 }
 
 
 //Routes
 server.get('/', homeHandler);
-server.get('/book',getBooksHandler);
-server.post('/books',addBook);
-server.delete('/books/:id',deleteBook);
+server.get('/book', getBooksHandler);
+server.post('/books', addBooks);
+server.delete('/books/:id', deleteBook);
+server.put('/updatebooks/:id', updatebookHandler);
 
 //Functions Handlers
-function homeHandler(req,res){
+function homeHandler(req, res) {
 
     res.send('Home Page');
 }
@@ -95,29 +272,24 @@ function getBooksHandler(req,res){
 
 
 
-async function addBook(req,res){
+async function addBooks(req, res) {
     console.log(req.body);
     const bookTitle = req.body.bookTitle;
     const bookDescription = req.body.bookDescription;
     const bookStatus = req.body.bookStatus;
-    const bookEmail = req.body.ownerEmail;
-    // const {bookTitle, bookDescription, bookStatus,bookEmail} = req.body;
-    await bookModel.create({ 
+    const bookEmail = req.body.bookEmail;
+    await bookModel.create({
         bookTitle: bookTitle,
         bookDescription: bookDescription,
         bookStatus: bookStatus,
-        bookEmail :bookEmail
+        bookEmail: bookEmail
     });
 
-    // await KittenModel.create({catName,catBreed,ownerEmail});
-
-    bookModel.find({ownerEmail:bookEmail},(err,result)=>{
-        if(err)
-        {
+    bookModel.find({ ownerEmail: bookEmail }, (err, result) => {
+        if (err) {
             console.log(err);
         }
-        else
-        {
+        else {
             res.send(result);
         }
     })
@@ -125,18 +297,17 @@ async function addBook(req,res){
 }
 
 
-function deleteBook(req,res){
+
+function deleteBook(req, res) {
     const bookId = req.params.id;
     const email = req.query.email;
-    bookModel.deleteOne({_id:bookId},(err,result)=>{
-        
-        bookModel.find({ownerEmail:email},(err,result)=>{
-            if(err)
-            {
+    bookModel.deleteOne({ _id: bookId }, (err, result) => {
+
+        bookModel.find({ ownerEmail: email }, (err, result) => {
+            if (err) {
                 console.log(err);
             }
-            else
-            {
+            else {
                 res.send(result);
             }
         })
@@ -148,8 +319,23 @@ function deleteBook(req,res){
 
 
 
+function updatebookHandler(req, res) {
+    const id = req.params.id;
+    const { bookTitle, bookDescription, bookStatus, bookEmail } = req.body;
+
+    bookModel.findByIdAndUpdate(id, { bookTitle, bookDescription, bookStatus, bookEmail }, (err, result) => {
+        bookModel.find({ ownerEmail: bookEmail }, (err, result) => {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                res.send(result);
+            }
+        })
+    })
+}
+
 
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`);
 })
-
